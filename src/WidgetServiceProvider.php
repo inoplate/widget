7137f2338ba9587ad57f8e6ace71a5867f9b0788
Roseffendi <carrpirates@gmail.com>
2016-06-18 03:08:21 +0700

    add singleton binding

--- src/WidgetServiceProvider.php
+++ src/WidgetServiceProvider.php
@@ -18,6 +18,7 @@ class WidgetServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->singleton('\Inoplate\Widget\Widget', 'Inoplate\Widget\Widget');
         $this->app->singleton('widget', function($app){
             return new Widget($app['cache.store']);
         });
